e0d448a33c35734774bc82754aa3b1ef4c679700
mikeylinton <michaeljlinton.ml@gmail.com>
2021-03-12 13:26:11 +0000

    DEV

--- honors-project-dev.py
+++ honors-project-dev.py
@@ -1,14 +1,15 @@
-import re;from graphviz import Digraph as Dot
+import re;from graphviz import Digraph as Dot; import argparse
 inputfile=open('dev.in','r')
 
-section=None
-players={}
-buildings={}
-PreConditions=[]
-MainFlow=[]
-PostConditions=[]
-PLACEHOLDER=None
+parser=argparse.ArgumentParser(description='Honors Project by Michael Linton.')
+parser.add_argument('--verbose','-v',action=argparse.BooleanOptionalAction,help='Verbose')
+args=parser.parse_args()
 
+PLACEHOLDER=None;section=None
+players={};buildings={}
+PreConditions=[];MainFlow=[];PostConditions=[]
+
+print("---------------")
 while True:
 	line=inputfile.readline().strip()
 	if not line:
@@ -34,20 +35,16 @@ while True:
 		items.remove('')
 		for item in items:
 			if section=="Environment":
-				building,rooms=item.split('[')
-				rooms=rooms.strip(']')
+				building,rooms=item.split('[');rooms=rooms.strip(']')
 				rooms=re.split(',',rooms)
-#				building=building.replace(' ', '_')
 				buildings[building]=[0,False,{}]
 				for room in rooms:
-#					room=room.replace(' ', '_')
 					buildings[building][2][room]=[0,False]
 
 			elif section=="PreConditions" or section=="MainFlow" or section=="PostConditions":
 				i=len(vars()[section])
 				vars()[section].append([])
-				action,elements=item.split('(')
-				elements=elements.strip(')')
+				action,elements=item.split('(');elements=elements.strip(')')
 				elements=re.split(',',elements)
 				vars()[section][i].append(action)
 				for element in elements:
@@ -55,52 +52,65 @@ while True:
 
 			elif section=="Players":
 				if re.search('^.*\[.*\]$',item):
-					head,tail=item.split('[')
-					messages=tail.strip(']')
-					player,authority=head.split('(')
-					authority=authority.strip(')')
+					head,tail=item.split('[');messages=tail.strip(']')
+					player,authority=head.split('(');authority=authority.strip(')')
 					messages=re.split(',',messages)
 				else:
-					player,authority=item.split('(')
-					authority=authority.strip(')')
+					player,authority=item.split('(');authority=authority.strip(')')
 					messages=None
 				players[player]=[authority,None,messages]
 
 	elif not re.search("^#",line):
 		print("\nUnexpected error:",line,'\n')
+if args.verbose:
+	print("buildings");print("---------------")
+	for building in buildings:
+		print(building,':',buildings[building])
+	print("---------------")
 
-#for building in buildings:
-#	print(building)
+if args.verbose:
+	print("players");print("---------------")
+	for player in players:
+		print(player,':',players[player])
+	print("---------------")
 
-#for player in players:
-#	print(player)
-
-print("------------------")
+if args.verbose: print("PreConditions");print("---------------")
 for string in PreConditions:
-	action=string.pop(0)
+	state=string.pop(0)
 
-	if action=="locked":
-		building=string.pop(0)
-		if re.search("\[.*\]",building):
-			building,room=building.split('[');room=room.strip(']')
-			buildings[building][2][room][1]=True
+	if state=="locked" or state=="unlocked":
+		facility=string.pop(0)
+		if state=="locked":
+			locked=True
 		else:
-			buildings[building][1]=True
-		print(buildings[building]);print("------------------")
+			locked=False
+		if re.search("\[.*\]",facility):
+			building,room=facility.split('[');room=room.strip(']')
+			buildings[building][2][room][1]=locked
+			if args.verbose: print(building,':',buildings[building])
+		else:
+			buildings[facility][1]=locked
+			if args.verbose: print(building,':',buildings[facility])
+		if args.verbose: print("---------------")
 	
-	if action=="unlocked":
-		building=string.pop(0)
-		if re.search("\[.*\]",building):
-			building,room=building.split('[');room=room.strip(']')
-			buildings[building][2][room][1]=False
+	if state=="location":
+		player=string.pop(0)
+		facility=string.pop(0)
+		players[player][1]=facility
+		if re.search("\[.*\]",facility):
+			building,room=facility.split('[');room=room.strip(']')
+			if int(buildings[building][2][room][0])==0:
+				buildings[building][2][room][0]=players[player][0]
 		else:
-			buildings[building][1]=False
-		print(buildings[building]);print("------------------")
+			if int(buildings[facility][0])==0:
+				buildings[facility][0]=players[player][0]
+		if args.verbose: print(player,':',players[player]);print("---------------")
+		
 
-print("MainFlow");print("------------------")
+if args.verbose: print("MainFlow");print("---------------")
 unit=0
 for string in MainFlow:
-	print(string)
+	if args.verbose: print(unit,string)
 	action=string.pop(0)
 
 	if action=="enter" or action=="exit" or action=="lock" or action=="unlock":
@@ -111,7 +121,7 @@ for string in MainFlow:
 		if re.search("\[.*\]",facility):
 			building,room=facility.split('[');room=room.strip(']')
 			
-			if action=="enter" and int(buildings[building][2][room][0])<1:
+			if action=="enter" and int(buildings[building][2][room][0])==0:
 				buildings[building][2][room][0]=players[player][0]	#Set Authority of room
 			
 			elif action=="exit":
@@ -122,15 +132,15 @@ for string in MainFlow:
 				if empty:
 					buildings[building][2][room][0]=0
 			
-			elif action=="lock":
-				PLACEHOLDER
-				if 
-			
-			elif action=="unlock":
-				PLACEHOLDER
+			elif (action=="lock" or action=="unlock") and int(buildings[building][2][room][0])<=int(players[player][0]):
+				if action=="lock":
+					buildings[building][2][room][1]=True
+				else:
+					buildings[building][2][room][1]=False
+				 
 		else:
 			
-			if action=="enter" and int(buildings[facility][0])<1:
+			if action=="enter" and int(buildings[facility][0])==0:
 				buildings[facility][0]=players[player][0]
 			
 			elif action=="exit":
@@ -141,63 +151,30 @@ for string in MainFlow:
 				if empty:
 					buildings[facility][0]=0
 			
-			elif action=="lock":
-				PLACEHOLDER
-			
-			elif action=="unlock":
-				PLACEHOLDER
-
+			elif (action=="lock" or action=="unlock") and int(buildings[facility][0])<=int(players[player][0]):
+				if action=="lock":
+					buildings[facility][1]=True
+				else:
+					buildings[facility][1]=False
+				
 		if action=="enter":
 			players[player][1]=facility
-		print(building,':',buildings[building]);print(player,':',players[player]);print("------------------")
+		if args.verbose: print(building,':',buildings[building]);print(player,':',players[player]);print("---------------")
 	
 	else:
-		print("Action not found");print("------------------")
-	unit+=1
-print("Total units:",unit)
-'''		
-	elif action=="enter":
-		player=string.pop(0)
-		facility=string.pop(0)
-		if re.search("\[.*\]",facility):
-			building,room=facility.split('[');room=room.strip(']')
-			if int(buildings[building][2][room][0])<1:
-				buildings[building][2][room][0]=players[player][0]
-		else:
-			if int(buildings[facility][0])<1:
-				buildings[facility][0]=players[player][0]
-		players[player][1]=facility
-		print(buildings[building]);print(players[player]);print("------------------")
+		print("Action not found");print("---------------")
 
-	elif action=="exit":
-		player=string.pop(0)
-		facility=string.pop(0)
-		empty=True
-		if re.search("\[.*\]",facility):
-			building,room=facility.split('[');room=room.strip(']')
-			players[player][1]=building	
-			for p in players:
-				if players[p][1]==facility:
-					empty=False;break
-			if empty:
-				buildings[building][2][room][0]=0
-		else:
-			players[player][1]=None
-			for p in players:
-				if players[p][1]==facility:
-					empty=False;break
-			if empty:
-				buildings[facility][0]=0
-		print(buildings[building]);print(players[player]);print("------------------")
-'''
-#		if facility in buildings:
-#			if int(buildings[facility][0])<1:
-#				buildings[facility][0]=players[player][0]
-#				print(buildings[facility])
-#		else:
-#			for building in buildings:
-#				if facility in buildings[building][2]:
-#					if int(buildings[building][2][facility][0])<1:
-#						buildings[building][2][facility][0]=players[player][0]
-#						print(buildings[building])
-#					break
+	if unit<10: print("Time",unit,"========")
+	elif unit<100: print("Time",unit,"=======")
+	else: print("Time",unit,"=======")
+	for player in players:
+		facility=players[player][1]
+		if facility is not None:
+			if re.search("\[.*\]",facility):
+				building,room=facility.split('[');room=room.strip(']')
+				print(player,'in',room,'in',building)
+			else:
+				print(player,'in',facility)
+	print("===============")
+	unit+=1
+print("Total units:",unit);print("---------------")
